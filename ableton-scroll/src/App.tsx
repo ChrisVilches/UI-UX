38c5fdce3e497296a71b1d2a1f7d92966634b4f4
Chris Vilches <felovilches@gmail.com>
2024-05-25 02:54:41 +0900

    first version ableton scroll

---
+++ ableton-scroll/src/App.tsx
@@ -0,0 +1,206 @@
+import { MutableRefObject, useCallback, useEffect, useRef, useState } from 'react'
+import './App.css'
+
+const COLS = Math.round(Math.random() * 50 + 50)
+const ROWS = 3
+
+function randomSet(n: number): Set<number> {
+  const res = new Set<number>()
+  for (let i = 0; i < n; i++) {
+    if (Math.random() > 0.8) {
+      res.add(i)
+    }
+  }
+  return res
+}
+// TODO: Make a button that randomizes the data, and also randomizes the N size.
+//       That way I can verify the data resizing works properly.
+const gridData = [
+  randomSet(COLS),
+  randomSet(COLS),
+  randomSet(COLS)
+]
+
+// TODO: Randomize colors. Create color set, shuffle, then pick the first N items. (that way they are all different)
+const dataColors = ['#ff0000', '#308782', '#F5DEB3']
+
+function compressRanges(arr: number[]) {
+  const groups: number[][] = []
+
+  arr.sort((a: number, b: number) => a - b)
+  for (const x of arr) {
+    if (groups.length === 0 || groups[groups.length - 1][1] + 1 !== x) {
+      groups.push([x, x])
+    }
+    groups[groups.length - 1][1] = x
+  }
+  return groups
+}
+
+const compressedGridData = gridData.map(s => compressRanges([...s.values()]))
+
+function Grid({ cellWidth }: { cellWidth: number }) {
+  const defaultBorderColor = '#7a7a7a'
+  return (
+    <>
+        {Array(ROWS).fill(null).map((_, rowIdx) => (
+          <div key={rowIdx} className="flex flex-cols">
+            {Array(COLS).fill(null).map((_, colIdx) => (
+              gridData[rowIdx].has(colIdx) ? (
+                <div key={colIdx} className="border-[1px] h-10" style={{
+                  borderTopColor: defaultBorderColor,
+                  borderBottomColor: defaultBorderColor,
+                  borderRightColor: gridData[rowIdx].has(colIdx + 1) ? dataColors[rowIdx] : defaultBorderColor,
+                  borderLeftColor: gridData[rowIdx].has(colIdx - 1) ? dataColors[rowIdx] : defaultBorderColor,
+                  minWidth: cellWidth,
+                  backgroundColor: dataColors[rowIdx]
+                }}></div>
+              ) : (
+                <div key={colIdx} className="border-[1px] bg-slate-400 h-10" style={{
+                  minWidth: cellWidth,
+                  borderColor: defaultBorderColor
+                }}></div>
+              )
+            ))}
+          </div>
+        ))}
+    </>
+  )
+}
+
+function GridPreview({ containerWidth, containerHeight }: { containerWidth: number, containerHeight: number }) {
+  const w = containerWidth / COLS
+  const h = containerHeight / ROWS
+  return (
+    <svg className="w-full">
+      {Array(ROWS).fill(null).map((_, rowIdx) => (
+        compressedGridData[rowIdx].map(([from, to]) => (
+          <rect key={from} y={rowIdx * h} height={h} width={w * (to - from + 1)} x={w * from} fill={dataColors[rowIdx]}></rect>
+        ))
+      ))}
+    </svg>
+  )
+}
+
+function App() {
+  const scrollContainerRef: MutableRefObject<HTMLDivElement | null> = useRef(null)
+  const scrollRef: MutableRefObject<HTMLDivElement | null> = useRef(null)
+  const contentRef: MutableRefObject<HTMLDivElement | null> = useRef(null)
+  const mousePos: MutableRefObject<{ x: number, y: number }> = useRef({ x: 0, y: 0 })
+  const dragging: MutableRefObject<boolean> = useRef(false)
+  const dragOffset: MutableRefObject<number> = useRef(0)
+  const scrollPercentage: MutableRefObject<number> = useRef(0)
+
+  const [containerWidth, setContainerWidth] = useState(0)
+  const [containerHeight, setContainerHeight] = useState(0)
+  const [cellWidth, setCellWidth] = useState(50)
+  const [scrollSize, setScrollSize] = useState(80)
+  // TODO: Implement zoom in/out using the Y movement (while dragging).
+  // TODO: Make the tiles more pretty.
+  // TODO: Make everything more pretty.
+  // TODO: When the scroll size increases (due to zoom change), it must not
+  //       get out of boundaries. Do a check to avoid that.
+  // TODO: Test with different datasets, and different window sizes.
+  // TODO: When the user resizes the screen, the scroll may get bigger than the container.
+  //       implement some logic so that the scroll never becomes bigger than the container.
+  // TODO: Scroll should increase in size whenever the window also gets enlarged.
+  // TODO: When the user clicks on the scrollbar, it should move to that position (maybe with animation?).
+
+  const scrollContent = useCallback(() => {
+    const contentWidth = contentRef.current!.scrollWidth
+    const scrollToValue = (contentWidth - contentRef.current!.getBoundingClientRect().width) * scrollPercentage.current
+    contentRef.current!.scrollLeft = scrollToValue
+    console.log(scrollPercentage.current)
+  }, [])
+
+  const resize = useCallback(() => {
+    setContainerHeight(scrollContainerRef.current!.getBoundingClientRect().height)
+    setContainerWidth(scrollContainerRef.current!.getBoundingClientRect().width)
+
+    const scrollWidth = scrollRef.current!.getBoundingClientRect().width
+    const containerWidth = scrollContainerRef.current!.getBoundingClientRect().width
+    const previewCellWidth = containerWidth / COLS
+
+    setCellWidth(previewCellWidth * containerWidth / scrollWidth)
+    scrollContent()
+  }, [scrollContent])
+
+  useEffect(() => {
+    const container = scrollContainerRef.current!
+    const scroll = scrollRef.current!
+
+    const sizeObserver = new ResizeObserver(resize)
+    resize()
+
+    // TODO: Clean
+    sizeObserver.observe(document.body)
+
+    document.addEventListener('mousemove', (e) => {
+      mousePos.current.x = e.clientX
+      mousePos.current.y = e.clientY
+
+      if (dragging.current) {
+        const offset = container.getBoundingClientRect().left
+        scroll.style.left = `${mousePos.current.x - offset - dragOffset.current}px`
+
+        if (scroll.getBoundingClientRect().right > container.getBoundingClientRect().right) {
+          const containerWidth = container.getBoundingClientRect().width
+          const scrollWidth = scroll.getBoundingClientRect().width
+          scroll.style.left = `${containerWidth - scrollWidth}px`
+        }
+
+        if (scroll.getBoundingClientRect().left < container.getBoundingClientRect().left) {
+          scroll.style.left = "0px"
+        }
+
+        scrollPercentage.current = (scroll.getBoundingClientRect().left - offset) / (container.getBoundingClientRect().width - scroll.getBoundingClientRect().width)
+        scrollContent()
+      }
+    })
+
+    // TODO: Almost works, but if the mouseup is outside the div, the event is not executed.
+    scroll.addEventListener('mousedown', function(e) {
+      dragging.current = true
+      dragOffset.current = e.offsetX
+    })
+
+    document.addEventListener('mouseup', function() {
+      dragging.current = false
+    })
+  }, [resize, scrollContent])
+
+  // TODO: Improve these functions.
+  const zoomIn = () => {
+    setScrollSize((s) => Math.min(160, s + 20))
+    resize()
+  }
+  const zoomOut = () => {
+    setScrollSize((s) => Math.max(80, s - 20))
+    resize()
+  }
+
+  return (
+    <div className="container mx-auto">
+      <div className="bg-slate-600 w-full h-20" ref={scrollContainerRef}>
+        <div className="absolute h-20">
+          <div className="bg-slate-800 hover:bg-slate-700 transition-colors duration-150 z-40 h-full relative opacity-80 select-none" style={{ width: scrollSize }} ref={scrollRef}></div>
+        </div>
+
+        <div className="h-20 select-none opacity-50">
+          <GridPreview containerWidth={containerWidth} containerHeight={containerHeight}/>
+        </div>
+      </div>
+
+      <div className="overflow-x-hidden select-none" ref={contentRef}>
+        <Grid cellWidth={cellWidth}/>
+      </div>
+
+      <div className="flex flex-row space-x-2 mt-4">
+        <button className="bg-green-800" onClick={zoomOut}>-</button>
+        <button className="bg-green-800" onClick={zoomIn}>+</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
