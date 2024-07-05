90b73530f171d4a7c592742520437abafbe4eec9
Chris Vilches <felovilches@gmail.com>
2024-07-05 20:04:34 +0900

    [sticky] add warning to mobile users

--- sticky/src/main.tsx
+++ sticky/src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { isMobile } from 'is-mobile'
+
+if (isMobile()) {
+  alert("This is not intended for mobile! It won't look right.")
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
